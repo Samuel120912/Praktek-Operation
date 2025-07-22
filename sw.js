e9c6ca5f9e621c0d418c3f4792467ec82380a49b
Samuel120912 <samuelsihombing11@gmail.com>
2025-07-22 15:19:38 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,13 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('form-cache-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png'
-      ]);
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('form-cache').then(cache => {
+      return cache.addAll(['index.html', 'manifest.json', 'icon-192.png']);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
