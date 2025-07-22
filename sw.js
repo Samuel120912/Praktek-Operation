2058fc1200a88bc41f7e921b51adbd7c66c742d7
Samuel120912 <samuelsihombing11@gmail.com>
2025-07-22 15:06:08 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-const CACHE_NAME = "pemeriksaan-cache-v1";
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png"
-];
-
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open('form-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png'
+      ]);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
